5b15b7415406a09b85181158b7c310d25336d16d
olek <aleksanderroztrop@gmail.com>
2019-04-10 23:08:31 +0200

    buf fixed v 1.1 for production

--- src/main/java/olek/com/MavenProject/NewClass.java
+++ src/main/java/olek/com/MavenProject/NewClass.java
@@ -2,6 +2,11 @@ package olek.com.MavenProject;
 
 public class NewClass {
 	public static void saySomething() {
-
+		int i = 2;
+		int j = 3;
+		while (i < j) {
+			System.out.println("i mniejsze od j");
+			i++;
+		}
 	}
 }
