17ae89d825174eea123c3e8f32be8c04c0905c85
olek <olek50000@o2.pl>
2019-04-10 23:40:51 +0200

    end coding in newclass

--- src/main/java/olek/com/MavenProject/NewClassNewClass.java
+++ src/main/java/olek/com/MavenProject/NewClassNewClass.java
@@ -2,6 +2,10 @@ package olek.com.MavenProject;
 
 public class NewClassNewClass extends NewClass{
 
-	
+	private void sout() {
+		// TODO Auto-generated method stub
+		System.out.println("aaa");
+
+	}
 	
 }
